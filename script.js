bb6c2f356559e8b19a4c629ba359d4a1f1170172
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-22 19:22:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let curtime=document.getElementById("timer");
+
+let date=new Date();
+
+setInterval(
+    function(){
+	    date=new Date();
+		curtime.innerHTML=date.toLocaleString();
+	},1000		
+	
+)
